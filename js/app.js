b8282bf82f0c89301c0b1ec3f6c91c116d7e83c4
Yunbo <puyunbo1999@outlook.com>
2023-08-06 12:13:21 +0800

    if-else

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 function isValidExpression(expression) {
-    const regex = /^[+\-*/0-9\s()]+$/;
-    return regex.test(expression);
+    const ifElseRegex = /^if\s*\((.*?)\)\s*{\s*(.*?)}\s*else\s*{\s*(.*?)}/;
+    const match = expression.match(ifElseRegex);
+    if (match) {
+        console.log(match)
+        const condition = match[1].trim();
+        const ifBlock = match[2].trim();
+        const elseBlock = match[3].trim();
+        // -> parse if
+        // if (x > 5) { result = 'x is greater than 5'; } else { result = 'x is not greater than 5'; }
+        const treeData = parseIfThenElse(condition, ifBlock, elseBlock);
+        new Treant(treeData);
+        return false;
+    } else {
+        const regex = /^[+\-*/0-9\s()]+$/;
+        return regex.test(expression);
+    }
 }
 
 function buildTree() {
     let expression = document.getElementById('expression').value;
     if (!isValidExpression(expression)) {
-        alert('Invalid expression! Please enter a valid math expression.');
         return;
     }
 
@@ -57,6 +70,35 @@ class NodeStructure {
     }
 }
 
+class Item {
+    constructor(tag, value) {
+        this.tag = tag;
+        this.value = value;
+    }
+}
+
+class Expression {
+    constructor(op, i1, i2) {
+        this.op = op;
+        this.i1 = i1;
+        this.i2 = i2;
+    }
+}
+
+var binop = [];
+
+// -> parse if
+function parseIfThenElse(condition, ifBlock, elseBlock) {
+    const conditionItem = new NodeStructure(condition);
+    conditionItem.children.push(new NodeStructure("true ->"));
+    conditionItem.children.push(new NodeStructure(ifBlock));
+    conditionItem.children.push(new NodeStructure("false ->"));
+    conditionItem.children.push(new NodeStructure(elseBlock));
+    chart_config.nodeStructure = conditionItem;
+    return chart_config;
+}
+
+// String -> AST
 function parseExpression(expression) {
     const arr = infixToPrefix(expression);
     const stack = [];
@@ -67,14 +109,18 @@ function parseExpression(expression) {
         if (!isOperator(token)) {
             // If the token is a number, create a leaf node and push it to the stack
             stack.push(new NodeStructure(token));
+            binop.push(new Item("int", token))
         } else {
             // If the token is an operator, create a new node, assign its left and right children from the stack, and push it back to the stack
             const operatorNode = new NodeStructure(token);
             operatorNode.children.push(stack.pop());
             operatorNode.children.push(stack.pop());
+            binop.push(new Item("op", token))
             stack.push(operatorNode);
         }
     }
+
+    console.log(binop);
 
     chart_config.nodeStructure = stack.pop();
     return chart_config;
